ed3ada63d781e5a8326fe6d4aa8d0b421d621d1f
Ryan-Perez <perezryanm@gmail.com>
2022-07-13 11:19:48 -0700

    update

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import authRoutes from "./routes/auth"
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
+const uri = "mongodb+srv://gutbole:ryanperez55@cluster0.9ojvt.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(
+    process.env.MONGO_URI as string
+)
+.then(() => {
+    console.log('Connected to Mongodb')
+
+    const app = express();
+
+    app.use(express.json())
+    app.use("/auth", authRoutes)
+
+    app.listen(8080, () => {
+        
+        console.log('Now listening to port 8080')
+    })
+})
+.catch((error) => {
+    console.log({error})
+    throw new Error(error)
+})
---
+++ server/src/routes/auth.ts
@@ -0,0 +1,123 @@
+import express from "express"
+import { body, validationResult } from "express-validator"
+import User from "../models/users"
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+
+
+const router = express.Router()
+
+router.post(
+    '/signup',
+    body("email").isEmail().withMessage("The emial is invalid"),
+    body("password").isLength({min: 5}).withMessage("The password is too short"),
+    async (req, res) => {
+
+        const validationErrors = validationResult(req)
+
+        if(!validationErrors.isEmpty()){
+            const errors: any = validationErrors.array().map(error => {
+                return {
+                    msg: error.msg
+                }
+            })
+
+            return res.json({errors, data: null})
+        }
+
+
+        const { email, password } = req.body
+        
+        const user: any = await User.findOne({ email })
+
+        if(user){
+            return res.json({ 
+                errors: [
+                    {
+                        msg: "Email already in use"
+                    },
+                ],
+                data: null    
+             })
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        const newUser: any = await User.create({
+            email,
+            password: hashedPassword
+        })
+
+        const token = await JWT.sign(
+            {email: newUser.email},
+            process.env.JWT_SECRET as string,
+            {
+                expiresIn: 36000,
+            }
+        )
+
+        res.json({
+            errors: [],
+            data: {
+                token,
+                user: {
+                    id: newUser._id,
+                    email: newUser.email,
+                },
+            },
+        })
+    }
+)
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+        return res.json({
+            errors: [
+                {
+                    msg: "Invalids credentials"
+                }
+            ],
+            data: null
+        })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+        return res.json({
+            errors: [
+                {
+                    msg: "Invalids credentials"
+                }
+            ],
+            data: null
+        })
+    }
+
+    const token = await JWT.sign(
+        {email: user.email},
+        process.env.JWT_SECRET as string,
+        {
+            expiresIn: 36000,
+        }
+    )
+
+    return res.json({
+        errors: [],
+        data: {
+            token,
+            user: {
+                id: user._id,
+                email: user.email,
+            }
+        }
+    })
+
+})
+
+
+export default router
